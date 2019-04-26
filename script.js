5c88255e3bd8d060442d9badc54d7eacccffc4a3
Guibibi <bgui34@gmail.com>
2019-04-26 17:04:31 -0400

    Added Carousel

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: true,
+    items: 1,
+    itemsDesktop: false,
+    itemsDesktopSmall: false,
+    itemsTablet: false,
+    itemsMobile: false
+  });
+});
